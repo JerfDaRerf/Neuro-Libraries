18e5501f07d0c235e37fb7142d11c774762a86a2
JerfDaRerf <125484581+JerfDaRerf@users.noreply.github.com>
2024-05-10 15:30:25 -0500

    Update FC-OneStage.py

--- PythonCode/FC-OneStage.py
+++ PythonCode/FC-OneStage.py
@@ -6,8 +6,6 @@ from torch import double, float64, nn
 from torch.nn import functional as F
 from torch.utils.data._utils.collate import default_collate
 
-from cs639.loading import *
-
 from torchvision import models, transforms, ops
 from torchvision.models import feature_extraction
 from torchvision.ops import sigmoid_focal_loss
